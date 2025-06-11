295e451e7d1a309313e185df73cac57d7a7d8cd6
nishindprojects <2326kb30@kgcas.com>
2025-06-11 12:31:17 +0530

    Add files via upload

---
+++ DAY 5 - TASK 2.java
@@ -0,0 +1,16 @@
+ class main {
+ public static void main(String [] args) {
+     try{
+         int [] mynumbers = {1,2,3};
+         System.out.println(mynumbers[10]);
+     }catch (Exception e){
+         System.out.println("Something goes wrong or error");
+     }
+     
+     {
+         System.out.println("the try catch is finished");
+     }
+     
+ }    
+ }
+ 
